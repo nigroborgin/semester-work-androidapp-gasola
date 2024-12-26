7fb6a00bfff7ba196b20a5f7c39ce23a63ef65e8
Vyacheslav Shkalin <vyacheslav.shkalin@moneta.ru>
2024-12-26 17:31:09 +0300

    refactor: Login Activity

--- app/src/main/java/ru/marsu/gasola/LoginActivity.kt
+++ app/src/main/java/ru/marsu/gasola/LoginActivity.kt
@@ -1,20 +1,17 @@
 package ru.marsu.gasola
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import ru.marsu.gasola.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }
